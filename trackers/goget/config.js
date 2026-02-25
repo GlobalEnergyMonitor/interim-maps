d8e1c4781517f6183b7d85e3de880e46379d0321
Hannah Howell <hannah.howell@globalenergymonitor.org>
2026-02-25 12:25:05 -0700

    Updated GOGET path

--- trackers/goget/config.js
+++ trackers/goget/config.js
@@ -1,6 +1,6 @@
 var config = {
     /* name of the data file; use key `csv` if data file is CSV format, use key `geojson` if data file is geoJSON format */
-    geojson: 'https://publicgemdata.nyc3.cdn.digitaloceanspaces.com/refactor_testing/goget_map_2026-02-24.geojson',
+    geojson: 'https://publicgemdata.nyc3.cdn.digitaloceanspaces.com/refactor_testing/goget_map_2026-02-25.geojson',
 
     /* Labels for describing the assets */
     assetFullLabel: 'Oil & Gas Extraction Areas',
