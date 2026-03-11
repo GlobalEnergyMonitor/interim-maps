63e9038a26d6f7b9a881cbb17a15de56933282a3
Hannah Howell <hannah.howell@globalenergymonitor.org>
2026-03-11 16:52:32 -0600

    Updated file names

--- trackers/gspt/config.js
+++ trackers/gspt/config.js
@@ -1,6 +1,6 @@
 var config = {
     /* name of the data file; use key `csv` if data file is CSV format, use key `geojson` if data file is geoJSON format */
-    geojson: 'https://publicgemdata.nyc3.cdn.digitaloceanspaces.com/refactor_testing/gspt_map_2026-02-24.geojson',
+    geojson: 'https://publicgemdata.nyc3.cdn.digitaloceanspaces.com/refactor_testing/gspt_map_2026-03-11.geojson',
 
     /* Labels for describing the assets */
     assetFullLabel: "Solar farm phases",
