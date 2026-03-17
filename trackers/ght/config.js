d79a9c3a1ff41de5e24e35f9eb5383f90f435437
Hannah Howell <hannah.howell@globalenergymonitor.org>
2026-03-17 12:41:21 -0600

    Updated hydro url

--- trackers/ght/config.js
+++ trackers/ght/config.js
@@ -1,6 +1,6 @@
 var config = {
     /* name of the data file; use key `csv` if data file is CSV format, use key `geojson` if data file is geoJSON format */
-    geojson: 'https://publicgemdata.nyc3.cdn.digitaloceanspaces.com/refactor_testing/ght_map_2026-02-24.geojson',
+    geojson: 'https://publicgemdata.nyc3.cdn.digitaloceanspaces.com/refactor_testing/ght_map_2026-03-17.geojson',
 
     /* Labels for describing the assets */
     assetFullLabel: "Hydropowered Stations",
