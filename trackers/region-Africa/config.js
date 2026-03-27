800eaafa89ef86e47279424fd69f110e86876947
Hannah Howell <hannah.howell@globalenergymonitor.org>
2026-03-27 17:42:11 -0600

    Updated config file for regional maps

--- trackers/region-Africa/config.js
+++ trackers/region-Africa/config.js
@@ -1,207 +1,105 @@
 var config = {
-    geojson: 'https://publicgemdata.nyc3.cdn.digitaloceanspaces.com/ggit/2025-11/africa-energy_map_2025-11-25.geojson', 
-    geometries: ['Point','LineString'],
-    center: [15, 0],
-    zoomFactor: 1,
-    statusField: 'status-legend',
-    statusDisplayField: 'status',
-    // linkField: 'id',
-    color: {
-        field: 'tracker-custom',
-        values: {
-            'GOGPT': 'blue',
-            'GOGET-oil': 'red',
-            'GOIT': 'green',
-            'GGIT': 'green',
-            'GGIT-import':'green',
-            'GGIT-export':'green',
-            'GCPT': 'blue',
-            'GCMT': 'red',
-            'GCTT': 'green',
-            'GBPT': 'blue',
-            'GGPT': 'blue',
-            'GNPT': 'blue',
-            'GSPT': 'blue',
-            'GWPT': 'blue',
-            'GHPT': 'blue'
-        }
-    },
-    //filter values should have no spaces
-    filters: [
-        {
-            field: 'tracker-custom',
-            values: ["GCPT", "GOGPT", "GBPT", "GNPT", "GSPT", "GWPT", "GHPT", "GGPT", "GOIT", "GGIT", "GGIT-import", "GGIT-export","GCTT", "GOGET-oil", "GCMT"], 
-            values_labels: ['coal units', 'oil&gas units', 'bioenergy units', 'nuclear units', 'solar phases', 'wind phases', 'hydropower plants', 'geothermal units', 'oil pipelines', 'gas pipelines', 'LNG import terminals', 'LNG export terminals', 'coal terminals', 'oil&gas extraction areas','coal mines'],
-            primary: true
-        },
-        {
-            field: 'status-legend',
-            label: 'Status',
-            values: ['operating','proposed-plus','pre-construction-plus','construction-plus','retired-plus','cancelled','mothballed-plus','shelved', 'not-found'],
-            values_labels: ['Operating','Proposed/Announced/Discovered','Pre-construction/Pre-permit/Permitted', 'Construction/In development','Retired/Closed/Decommissioned','Cancelled','Mothballed/Idle/Shut in','Shelved', 'Not Found']
+    /* name of the data file; use key `csv` if data file is CSV format, use key `geojson` if data file is geoJSON format */
+    geojson: 'https://publicgemdata.nyc3.cdn.digitaloceanspaces.com/refactor_testing/africa_map_2026-03-27.geojson',
 
-        },
-
-    ],
-    capacityField: 'scaling-capacity',
-    
-    capacityDisplayField: 'capacity-table',
-
-
-    //can be string for single value, or hash. always single value is showMaxCapacity is true
-    capacityLabel: {
-        field: 'tracker-custom',
-        values: {
-            'GCPT': 'MW',
-            'GOGPT': 'MW',
-            'GBPT':	'MW',
-            'GNPT':	'MW',
-            'GSPT':	'MW',
-            'GSPT':	'MW',
-            'GWPT':	'MW',
-            'GHPT':	'MW',
-            'GGPT':	'MW',
-            'GOGET-oil':	'million boe/y', // remove because they dont have capacity is only relevant for scaling here Scott request
-            'GOIT': 'boe/d',
-            'GGIT':	'bcm/y of natural gas',
-            'GGIT-import': 'MTPA of natural gas',
-            'GGIT-export': 'MTPA of natural gas',            
-            'GCMT':	'million tonnes coal/y', 
-            'GCTT':	'million tonnes coal/y'
-        }
-    },
-    // skipCapacitySum: '',
-
-    // capItemLabel:  {
-    //         field: 'tracker-custom',
-    //         values: {
-    //             'GCPT': 'MW',
-    //             'GOGPT': 'MW',
-    //             'GBPT':	'MW',
-    //             'GNPT':	'MW',
-    //             'GSPT':	'MW',
-    //             'GWPT':	'MW',
-    //             'GHPT':	'MW',
-    //             'GGPT':	'MW',
-    //             // 'GOGET - oil':	'million boe/y',
-    //             // 'GOGET - gas':	'million m³/y',
-    //             'GOIT': 'boe/d',
-    //             'GGIT':	'Bcm/y of natural gas',
-    //             'GGIT - import': 'MTPA of natural gas',
-    //             'GGIT - export': 'MTPA of natural gas',
-    //             // 'GCMT':	'million tonnes coal/y',
-    //             'GCTT':	'million tonnes coal/y'
-    //         }
-    //     },
-    // prodItemLabel: {
-    //     field: 'tracker-custom',
-    //         values: {
-    //             'GOGET - oil':	'million boe/y',
-    //             'GOGET - gas':	'million m³/y',
-    //             'GCMT':	'million tonnes coal/y'
-    //         }
-    // },
-    //productionLabel NEED a productionLabel
-    showMaxCapacity: false,
-
-    assetFullLabel: "Units / Phases / Pipelines", 
+    /* Labels for describing the assets */
+    assetFullLabel: 'Units / Phases / Pipelines',
     assetLabel: 'units',
 
-    // can be string for single value, or hash
-    // assetLabel: {
-    //     field: 'tracker-custom',
-    //     values: {
-    //         'GCPT': 'units',
-    //         'GOGPT': 'units',
-    //         'GBPT': 'units',
-    //         'GNPT': 'units',
-    //         'GSPT': 'phases',
-    //         'GWPT':	'phases',
-    //         'GHPT':	'units',
-    //         'GGPT':	'units',
-    //         'GOGET - oil': 'areas',
-    //         'GOGET - gas': 'areas',
-    //         'GOIT': 'projects',
-    //         'GGIT': 'projects',
-    //         'GGIT - import': 'projects',
-    //         'GGIT - export': 'projects',
-    //         'GCMT': 'projects',
-    //         'GCTT': 'projects'
-    //     }
-    // },
-    nameField: 'name',
-    countryFile: 'countries.js',
-    allCountrySelect: true, // TODO bug when clicking Africa nothing gets selected but clicking all it does 
-    countryField: 'areas',
-    //if multicountry, always end values with a comma
-    multiCountry: true,
-
+    /* configure the table view, selecting which columns to show, how to label them,
+       and designated which column has the link */
     tableHeaders: {
-        values: ['name','unit-name', 'owner', 'parent', 'capacity-table', 'units-of-m','status', 'areas', 'start-year', 'prod-oil', 'prod-year-oil', 'prod-gas', 'prod-year-gas', 'prod-coal','tracker-display',],
-        labels: ['Name','Unit','Owner', 'Parent','Capacity', '','Status','Country/Area(s)','Start year', 'Production (million bbl/y)','Production year (oil)', 'Production (Million m³/y)', 'Production year (gas)', 'Production (Mt)','Facility Type'],
-
+        values: ['name', 'unit-name', 'owner', 'parent', 'capacity-table', 'units-of-m', 'status', 'all-countries', 'start-year', 'prod-oil', 'prod-year-oil', 'prod-gas', 'prod-year-gas', 'prod-coal', 'tracker-display',],
+        labels: ['Name', 'Unit', 'Owner', 'Parent', 'Capacity', '', 'Status', 'Country/Area(s)', 'Start year', 'Production Oil (million bbl/y)', 'Production year (oil)', 'Production Gas (Million m³/y)', 'Production year (gas)', 'Production Coal (Mt)', 'Facility Type'],
         clickColumns: ['name'],
-        rightAlign: ['unit','capacity-table','prod-oil', 'prod-gas','start-year', 'prod-year-oil', 'prod-year-gas'], 
-        removeLastComma: ['areas'], 
-        toLocaleString: ['capacity-table'],
-        // displayValue: {'tracker-display': "assetLabel"},
-        // appendValue: {'capacity': "capItemLabel"},
-        // appendValue: {'production': "prodItemLabel"},
-        // appendValue: {'capacity-oil ': "prodItemLabel"},
-        // appendValue: {'capacity-gas': "prodItemLabel"},
-
+        rightAlign: ['unit-name', 'capacity-table', 'prod-oil', 'prod-gas', 'start-year', 'prod-year-oil', 'prod-year-gas'],
     },
-    searchFields: { 'Project': ['name'], 
-        'Companies': ['owner', 'parent'],
+
+    /* configure the search box;
+       each label has a value with the list of fields to search. Multiple fields might be searched */
+    searchFields: {
+        'Project': ['name', 'name-search', 'project-id'],
+        'Companies': ['owner', 'parent', 'owner-search', 'parent-search'],
         'Start Year': ['start-year'],
         'Infrastructure Type': ['tracker-display'],
-        'Status': ['status'],
-        'Province/State': ['subnat']
+        'Status': ['status-display'],
+        'Province/State': ['subnational']
     },
 
-    // combine name and unit-name into one variable so that it can be the heading of the detail view card
-    
-
+    /* define fields and how they are displayed.
+      `'display': 'heading'` displays the field in large type
+      `'display': 'range'` will show the minimum and maximum values.
+      `'display': 'join'` will join together values with a comma separator
+      `'display': 'location'` will show the fields over the detail image
+      `'label': '...'` prepends a label. If a range, two values for singular and plural.
+    */
     detailView: {
         'name': {'display': 'heading'},
-        'prod-oil': {'label': 'Production (Million bbl/y)'},
-        'prod-gas': {'label': 'Production (Million m³/y)'},
+        'location-display': {'display': 'location'},
+
+        'prod-oil': {'label': 'Liquids Production', 'trailing-label': 'million bbl/y'},
         'prod-year-oil': {'label': 'Production Year - Oil'},
+        'prod-gas': {'label': 'Gas Production', 'trailing-label': 'million m³/y'},
         'prod-year-gas': {'label': 'Production Year - Gas'},
-        'prod-coal': {'label': 'Production (Mt)'},
+        'prod-unspecified': {'label': 'Unspecified Hydrocarbons Production', 'trailing-label': 'million boe/y'},
+        'prod-year-unspecified': {'label': 'Production Year - Hydrocarbons (unspecified)'},
+        'prod-coal': {'label': 'Production - Coal', 'trailing-label': 'million tonnes coal/y'},
+
         'start-year': {'label': 'Start Year'},
         'owner': {'label': 'Owner'},
         'parent': {'label': 'Parent'},
-        'river': {'label': 'River'},
+        'river': {'label': 'River/Watercourse'},
+
         'tracker-display': {'label': 'Type'},
-        'areas-subnat-sat-display': {'display': 'location'}, 
     },
 
-    // /* radius associated with minimum/maximum value on map */
-    // minRadius: 2,
-    // maxRadius: 10,
-    // minLineWidth: 1,
-    // maxLineWidth: 10,
+    /* ---------------------------- FIELDS TO OVERWRITE FROM site-config.js ---------------------------- */
 
-    // /* radius to increase min/max to under high zoom */
-    // highZoomMinRadius: 4,
-    // highZoomMaxRadius: 32,
-    // highZoomMinLineWidth: 4,
-    // highZoomMaxLineWidth: 32,
+    color_association: {
+        field: 'tracker-display',
+        values: {
+            'Coal power station': 'blue',
+            'Oil & gas power station': 'blue',
+            'Bioenergy power station': 'blue',
+            'Nuclear power plant': 'blue',
+            'Solar power station': 'blue',
+            'Wind power station': 'blue',
+            'Hydropower plant': 'blue',
+            'Geothermal power plant': 'blue',
 
-    /* radius associated with minimum/maximum value on map */
-    minRadius: .8,
-    maxRadius: 10,
-    // /* radius to increase min/max to under high zoom */
-    highZoomMinRadius: 4,
-    highZoomMaxRadius: 32,
+            'Oil pipeline': 'green',
+            'Gas pipeline': 'green',
+            'LNG import terminal': 'green',
+            'LNG export terminal': 'green',
+            'Coal terminal': 'green',
 
-    minLineWidth: .4,
-    maxLineWidth: 7,
-    highZoomMinLineWidth: .4,
-    highZoomMaxLineWidth: 7,
-    // showAllPhases: true
-    
-    // interpolate: ["cubic-bezier", 0, 0, 0, 1],
+            'Oil & gas extraction area': 'red',
+            'Coal mine': 'red',
+        }
+    },
 
-};
+    filters: [
+        {
+            field: 'tracker-display',
+            values: ['Coal power station', 'Oil & gas power station', 'Bioenergy power station', 'Nuclear power plant', 'Solar power station', 'Wind power station', 'Hydropower plant', 'Geothermal power plant', 'Oil pipeline', 'Gas pipeline', 'LNG import terminal', 'LNG export terminal', 'Coal terminal', 'Oil & gas extraction area', 'Coal mine'],
+            primary: true
+        },
+        {
+            field: 'status-group',
+            label: 'Status',
+            values: ['operating', 'proposed-plus', 'pre-construction-plus', 'construction-plus', 'retired-plus', 'cancelled', 'mothballed-plus', 'shelved', 'not-found'],
+            values_labels: ['Operating', 'Proposed/Announced/Discovered', 'Pre-construction/Pre-permit/Permitted', 'Construction/In development', 'Retired/Closed/Decommissioned', 'Cancelled', 'Mothballed/Idle/Shut in', 'Shelved', 'Not Found']
+        },
+    ],
+
+    countryField: 'all-countries',
+    // countryFile: 'countries.json',  // fixme
+
+    geometries: ['Point', 'LineString'],
+    center: [15, 0],
+
+    minLineWidth: 1,
+    maxLineWidth: 3,
+    highZoomMinLineWidth: 1,
+    highZoomMaxLineWidth: 3,
+}
--- trackers/region-Asia/config.js
+++ trackers/region-Asia/config.js
@@ -1,132 +1,92 @@
 var config = {
+    /* name of the data file; use key `csv` if data file is CSV format, use key `geojson` if data file is geoJSON format */
+    geojson: 'https://publicgemdata.nyc3.cdn.digitaloceanspaces.com/refactor_testing/asia_map_2026-03-27.geojson',
 
-    geojson: 'https://publicgemdata.nyc3.cdn.digitaloceanspaces.com/ggit/2025-11/asia_map_2025-11-25.geojson',
-    geometries: ['Point','LineString'],
-    center: [60, 20],
-    zoomFactor: 1.9,
-    img_detail_zoom: 10,
-    statusField: 'status-legend',
-    statusDisplayField: 'status',
-    // allCountrySelect: false,
-
-    color: {
-        field: 'tracker-custom',
-        values: {
-            'GOGPT': 'blue',
-            'GOGET-oil': 'red',
-            'GGIT': 'green',
-            'GGIT-import':'green',
-            'GGIT-export':'green',
-
-
-        }
-    },
-    
-
-    filters: [
-        {
-            field: 'tracker-custom',
-            values: ["GOGPT",  "GGIT", "GGIT-import", "GGIT-export","GOGET-oil", ], 
-            values_labels: ['gas power units', 'gas pipelines', 'LNG import terminals', 'LNG export terminals', 'gas extraction areas',], // CHECK THAT
-            primary: true
-        },
-        {
-            field: 'status-legend',
-            label: 'Status',
-            values: ['operating','proposed-plus','pre-construction-plus','construction-plus','retired-plus','cancelled','mothballed-plus','shelved', 'not-found'],
-            values_labels: ['Operating','Proposed/Announced/Discovered','Pre-construction/Pre-permit/Permitted', 'Construction/In development','Retired/Closed/Decommissioned','Cancelled','Mothballed/Idle/Shut in','Shelved', 'Not Found']
-        },
-
-    ],
-    capacityField: 'scaling-capacity',
-    
-    capacityDisplayField: 'capacity-table',
-    capacityLabel: {
-        field: 'tracker-custom',
-        values: {
-            'GOGPT': 'MW',
-            // 'GOGET-oil':	'million boe/y', // remove because they dont have capacity is only relevant for scaling here Scott request
-            'GGIT':	'bcm/y of natural gas',
-            'GGIT-import': 'MTPA of natural gas',
-            'GGIT-export': 'MTPA of natural gas',            
-
-        }
-    },
-    showMaxCapacity: false,
-
-    assetFullLabel: "Units / Pipelines", 
-    //can be string for single value, or hash
-    // not using assetLabel for now TODO
+    /* Labels for describing the assets */
+    assetFullLabel: 'Units / Pipelines',
     assetLabel: 'units',
-    // assetLabel: {
-        // field: 'tracker-custom',
-        // values: {
-        //     'GCPT': 'units',
-        //     'GOGPT': 'units',
-        //     'GBPT': 'units',
-        //     'GNPT': 'units',
-        //     'GSPT': 'phases',
-        //     'GWPT':	'phases',
-        //     'GHPT':	'units',
-        //     'GGPT':	'units',
-        //     'GOGET - oil': 'areas',
-        //     'GOGET - gas': 'areas',
-        //     'GOIT': 'projects',
-        //     'GGIT': 'projects',
-        //     'GGIT - import': 'projects',
-        //     'GGIT - export': 'projects',
-        //     'GCMT': 'projects',
-        //     'GCTT': 'projects'
-        // }
-    // },
-    nameField: 'name',
-    countryFile: 'countries.js',
-    countryField: 'areas',
-    //if multicountry, always end values with a comma
-    multiCountry: true,
 
+    /* configure the table view, selecting which columns to show, how to label them, 
+       and designated which column has the link */
     tableHeaders: {
-        values: ['name','unit-name','owner', 'parent', 'capacity-table', 'units-of-m','status', 'areas', 'start-year',  'prod-gas', 'prod-year-gas', 'fuel', 'tracker-display',],
-        labels: ['Name','Unit','Owner', 'Parent','Capacity', '','Status','Country/Area(s)','Start year', 'Production (Million m³/y)', 'Production year (gas)','Fuel', 'Facility Type'],
-
+        values: ['name', 'unit-name', 'owner', 'parent', 'capacity-table', 'units-of-m', 'status', 'all-countries', 'start-year', 'prod-gas', 'prod-year-gas', 'fuel', 'tracker-display',],
+        labels: ['Name', 'Unit', 'Owner', 'Parent', 'Capacity', '', 'Status', 'Country/Area(s)', 'Start year', 'Production (Million m³/y)', 'Production year', 'Fuel', 'Facility Type'],
         clickColumns: ['name'],
-        rightAlign: ['unit','capacity', 'prod-gas','start-year','prod-year-gas'], 
-        removeLastComma: ['areas'], 
-        toLocaleString: ['capacity'],
-  
+        rightAlign: ['unit-name', 'capacity-table', 'start-year', 'prod-gas', 'prod-year-gas'],
     },
-    searchFields: { 'Project': ['name'], 
-        'Companies': ['owner', 'parent'],
+    
+    /* configure the search box; 
+       each label has a value with the list of fields to search. Multiple fields might be searched */
+    searchFields: {
+        'Project': ['name', 'name-search', 'project-id'],
+        'Companies': ['owner', 'parent', 'owner-search', 'parent-search'],
         'Start Year': ['start-year'],
         'Infrastructure Type': ['tracker-display'],
-        'Status': ['status'],
-        'Province/State': ['subnat']
+        'Status': ['status-display'],
+        'Province/State': ['subnational']
     },
+    
+    /* define fields and how they are displayed. 
+      `'display': 'heading'` displays the field in large type
+      `'display': 'range'` will show the minimum and maximum values.
+      `'display': 'join'` will join together values with a comma separator
+      `'display': 'location'` will show the fields over the detail image
+      `'label': '...'` prepends a label. If a range, two values for singular and plural.
+    */
     detailView: {
         'name': {'display': 'heading'},
-        'prod-gas': {'label': 'Production (Million m³/y)'},
+        'location-display': {'display': 'location'},
+
+        'location-accuracy': {'label': 'Location Accuracy'},
+        'prod-gas': {'label': 'Gas Production', 'trailing-label': 'million m³/y'},
         'prod-year-gas': {'label': 'Production Year - Gas'},
+        'prod-unspecified': {'label': 'Unspecified Hydrocarbons Production', 'trailing-label': 'million boe/y'},
+        'prod-year-unspecified': {'label': 'Production Year - Hydrocarbons (unspecified)'},
         'start-year': {'label': 'Start Year'},
         'owner': {'label': 'Owner'},
         'parent': {'label': 'Parent'},
-        'river': {'label': 'River'},
+
         'tracker-display': {'label': 'Type'},
-        'areas': {'label': 'Country/Area(s)'},
-        'areas-subnat-sat-display': {'display': 'location'}, 
+        'all-countries': {'label': 'Country/Area(s)'},
     },
 
-        /* radius associated with minimum/maximum value on map */
-    // minRadius: 2,
-    // maxRadius: 10,
+    /* ---------------------------- FIELDS TO OVERWRITE FROM site-config.js ---------------------------- */
+    
+    color_association: {
+        field: 'tracker-display',
+        values: {
+            'Oil & gas power station': 'blue',
+            'Gas pipeline': 'green',
+            'LNG import terminal': 'green',
+            'LNG export terminal': 'green',
+            'Oil & gas extraction area': 'red',
+        }
+    },
+    
+    filters: [
+        {
+            field: 'tracker-display',
+            values: ['Oil & gas power station', 'Gas pipeline', 'LNG import terminal', 'LNG export terminal', 'Oil & gas extraction area'],
+            primary: true
+        },
+        {
+            field: 'status-group',
+            label: 'Status',
+            values: ['operating', 'proposed-plus', 'pre-construction-plus', 'construction-plus', 'retired-plus', 'cancelled', 'mothballed-plus', 'shelved', 'not-found'],
+            values_labels: ['Operating', 'Proposed/Announced/Discovered', 'Pre-construction/Pre-permit/Permitted', 'Construction/In development', 'Retired/Closed/Decommissioned', 'Cancelled', 'Mothballed/Idle/Shut in', 'Shelved', 'Not Found']
+        },
+    ],
+
+    geometries: ['Point', 'LineString'],
+    center: [60, 20],
+    zoomFactor: 1.9,
+    img_detail_zoom: 10,
+
+    countryField: 'all-countries',
+    // countryFile: 'countries.json',  // fixme
+
     minLineWidth: 1,
     maxLineWidth: 3,
-
-    // /* radius to increase min/max to under high zoom */
-    // highZoomMinRadius: 4,
-    // highZoomMaxRadius: 32,
-    // highZoomMinLineWidth: 4,
-    // highZoomMaxLineWidth: 32,
-    
-    // showAllPhases: true
-
-};
+    highZoomMinLineWidth: 1,
+    highZoomMaxLineWidth: 3,
+}
