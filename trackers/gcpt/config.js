96dc6d52bc904b38be989f83879a38e902d61691
Hannah Howell <hannah.howell@globalenergymonitor.org>
2026-08-03 15:06:54 -0600

    Updated for gcpt release

--- trackers/gcpt/config.js
+++ trackers/gcpt/config.js
@@ -1,6 +1,6 @@
 var config = {
     /* name of the data file; use key `csv` if data file is CSV format, use key `geojson` if data file is geoJSON format */
-    geojson: 'https://publicgemdata.nyc3.cdn.digitaloceanspaces.com/interim_maps/gcpt_map_2026-01.geojson',
+    geojson: 'https://publicgemdata.nyc3.cdn.digitaloceanspaces.com/interim_maps/gcpt_map_2026-08.geojson',
 
     /* Labels for describing the assets */
     assetFullLabel: "Coal-fired Units",
