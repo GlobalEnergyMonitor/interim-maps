90c825aa00da08b66db9bb8d9ef56c48a4a22032
Hannah Howell <hannah.howell@globalenergymonitor.org>
2026-08-07 15:15:20 -0600

    Updated GIPT input file

--- trackers/integrated-power/config.js
+++ trackers/integrated-power/config.js
@@ -8,12 +8,11 @@ var config = {
                --no-feature-limit --no-tile-size-limit --no-tile-compression \
                -y project-id -y name -y asset-type -y status -y all-countries -y capacity-scaled \
                integrated-power/integrated_map_2026-05-06.geojson */
-    tiles: ['https://publicgemdata.nyc3.cdn.digitaloceanspaces.com/integrated-power/2026-05/tiles/{z}%5C{x}%5C{y}.pbf'],
+    tiles: ['https://publicgemdata.nyc3.cdn.digitaloceanspaces.com/Current_maps/integrated-power/2026-08/tiles/{z}%5C{x}%5C{y}.pbf'],
     tileSourceLayer: 'integrated',
 
     /* name of the data file; use key `csv` if data file is CSV format, use key `geojson` if data file is geoJSON format */
-    // geojson: 'https://publicgemdata.nyc3.cdn.digitaloceanspaces.com/refactor_testing/integrated_map_2026-05-06.geojson',
-    csv: 'https://publicgemdata.nyc3.cdn.digitaloceanspaces.com/integrated-power/2026-05/integrated_map_2026-05-06.csv',
+    csv: 'https://publicgemdata.nyc3.cdn.digitaloceanspaces.com/Current_maps/integrated-power/2026-08/integrated_map_2026-08.csv',
 
     /* Labels for describing the assets */
     assetFullLabel: 'units / phases',
